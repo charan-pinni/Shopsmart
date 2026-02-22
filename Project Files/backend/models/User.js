d4dccdf66797d88db5a57a1bb2f38b88aeca871f
charanpinni <pinniboyinacharan@gmail.com>
2026-02-22 17:26:49 +0530

    Add files via upload

---
+++ Project Files/backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+}, { timestamps: true });
+
+// Hash password before saving
+UserSchema.pre("save", function (next) {
+    if (!this.isModified("password")) return next();
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err);
+            this.password = hash;
+            next();
+        });
+    });
+});
+
+module.exports = mongoose.model("User", UserSchema);
